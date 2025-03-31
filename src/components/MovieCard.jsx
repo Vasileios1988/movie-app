1dd6250dafb2b9de5b0038ce24d91ca90848d835
Vasileios Milias <v.milias1@gmail.com>
2025-03-31 16:39:19 +0100

    add of MovieCard component

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,23 @@
+
+export default function MovieCard({movie}){
+
+     function onFaveClick(){
+         alert("fave btn is clicked");
+     }
+    return(
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.poster} alt={movie.title}/>
+                <div className="movie-overlay">
+                    <button className="fave-btn" onClick={ onFaveClick}>♡</button>
+
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.info}</p>
+                <p>{movie.releaseDate}</p>
+            </div>
+        </div>
+    )
+}
